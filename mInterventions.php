5a0baba38980897ee48919d0fc6c860387eef4f4
EikyouPH <bastien.zagala@viacesi.fr>
2024-08-19 17:51:22 +0200

    modif page de modification

--- mInterventions.php
+++ mInterventions.php
@@ -1,5 +1,56 @@
 <?php
 require ('./smartyHeader.php');
+session_start();
 
-$smarty->display('mInterventions.tpl')
-?>
+if (isset($_SESSION["user_id"])){
+    try {
+        require_once "database.php";
+        $sql = "SELECT * FROM interventions WHERE ID_inter = {$_GET["id"]}";
+
+    $result = $pdo->query($sql);
+    $intervention = $result->fetch();
+    $id = $_GET["id"];
+    $nom = $intervention["nom_inter"];
+    $date = date("d/m/Y", strtotime($intervention['date_inter']));
+    $lieu = $intervention["lieu_inter"];
+    if ($intervention["type_inter"] == 1){
+        $type1 = "checked";
+        $smarty->assign('type1', $type1);
+    }
+    elseif($intervention["type_inter"] == 2){
+        $type2 = "checked";
+        $smarty->assign('type2', $type2);
+    }
+    elseif($intervention["type_inter"] == 3){
+        $type2 = "checked";
+        $smarty->assign('type3', $type3);
+    }
+    $desc = $intervention["desc_inter"];
+    if($intervention["emp_1"]!=NULL){
+        $check1="checked";
+        $smarty->assign('check1', $check1);
+    }
+
+    if($intervention["emp_2"]!=NULL){
+        $check2="checked";
+        $smarty->assign('check2', $check2);
+    }
+
+    if($intervention["emp_3"]!=NULL){
+        $check3="checked";
+        $smarty->assign('check3', $check3);
+    }
+
+    $smarty->assign('nom', $nom);
+    $smarty->assign('lieu', $lieu);
+    $smarty->assign('date', $date);
+    $smarty->assign('description', $desc);
+    $smarty->assign('id', $id);
+    $smarty->display('mInterventions.tpl');
+
+    
+
+    } catch (PDOExecption $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+}
