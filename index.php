ab8b6d679e5508aaebeeb1e50e985bf368f38c6b
EikyouPH <bastien.zagala@viacesi.fr>
2024-08-19 17:12:33 +0200

    Mise a jour acceuil

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
+
+session_start();
+
 require ('./smartyHeader.php');
 
-$smarty->display('index.tpl');
+if (isset($_SESSION["user_id"])){
+try {
+    require_once "database.php";
+
+    $sql = "SELECT * FROM utilisateurs
+            WHERE ID_user = {$_SESSION["user_id"]}";
+
+    $result = $pdo->query($sql);
+
+
+    $user = $result->fetch();
+    $smarty->assign('user',$user["nom_user"]);
+    $smarty->display('index.tpl');
+} catch (PDOExecption $e) {
+    die("Query failed: " . $e->getMessage());
+}
+    
+    
+}
+else{
+    header("Location: signin.php");
+    exit;
+} 
 ?>
