f749094f42eba018c095f2e68ae6f99f01c9fc43
EikyouPH <bastien.zagala@viacesi.fr>
2024-08-19 17:14:05 +0200

    Passage a PDO

--- listInterventions.php
+++ listInterventions.php
@@ -1,19 +1,37 @@
 <?php
 require ('./smartyHeader.php');
 
-$interventions = array(
-    "intervention1"=> array (
-        "nom" => "Taille Norma Forbach",
-        "lieu" => "Forbach",
-        "date" => "15/08/24"
-    ),
-    "intervention2"=> array (
-        "nom" => "Tonte KFC Feguersheim",
-        "lieu" => "Feguersheim",
-        "date" => "16/08/24"
-    )
-);
+session_start();
 
-$smarty->assign('interventions',$interventions);
-$smarty->display('listInterventions.tpl')
-?>
+try {
+    require_once "database.php";
+
+    $sql = "SELECT * FROM interventions";
+
+    $result = $pdo->query($sql);
+    $inters = array ();
+    $rows = $pdo->query('select count(*) from interventions')->fetchColumn();
+    if ($rows > 0) {
+        while($row = $result->fetch()) {
+            $inters[] = $row;
+        }
+    }
+    
+    //print_r($inters);
+    
+    foreach ($inters as $inter){
+        $intervention[] = array (
+            "id" => $inter['ID_inter'],
+            "nom" => $inter['nom_inter'],
+            "lieu" => $inter['lieu_inter'],
+            "date" => date("d/m/Y", strtotime($inter['date_inter']))
+        );
+    }
+    
+    $smarty->assign('interventions',$intervention);
+    $smarty->display('listInterventions.tpl');
+} catch (PDOExecption $e) {
+    die("Query failed: " . $e->getMessage());
+}
+
+
