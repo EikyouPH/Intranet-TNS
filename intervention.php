46f87c8f74ff6a97d349d044993fa6ea0a24a831
EikyouPH <bastien.zagala@viacesi.fr>
2024-09-21 15:38:20 +0900

    Recuperation dynamique du type

--- intervention.php
+++ intervention.php
@@ -6,7 +6,10 @@ session_start();
 if (isset($_SESSION["user_id"])){
     try {
         require_once "database.php";
-        $sql = "SELECT * FROM interventions WHERE ID_inter = {$_GET["id"]}";
+        $sql = "SELECT nom_inter, date_inter, lieu_inter, nom_type, desc_inter, emp_1, emp_2, emp_3
+                FROM interventions 
+                INNER JOIN type_inter ON interventions.type_inter = type_inter.ID_type
+                WHERE ID_inter = {$_GET["id"]}";
 
         $result = $pdo->query($sql);
         $intervention = $result->fetch();
@@ -14,15 +17,7 @@ if (isset($_SESSION["user_id"])){
         $nom = $intervention["nom_inter"];
         $date = date("d/m/Y", strtotime($intervention['date_inter']));
         $lieu = $intervention["lieu_inter"];
-        if ($intervention["type_inter"] == 1){
-            $type = "Taille";
-        }
-        elseif($intervention["type_inter"] == 2){
-            $type = "Tonte";
-        }
-        elseif($intervention["type_inter"] == 3){
-            $type = "Balayage";
-        }
+        $type = $intervention["nom_type"];
         $desc = $intervention["desc_inter"];
         $emp_1 = $intervention["emp_1"];
         $emp_2 = $intervention["emp_2"];
